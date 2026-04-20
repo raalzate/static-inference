976115e3abf4e09ca16fdd1f6fe1a97c93cb3969
raul andres alzate gomez <raul.alzate@sofka.com.co>
2026-04-20 18:42:10 -0500

    resolves

--- test-for-demo/dotnet-ecommerce/src/ECommerceApp/obj/Debug/net8.0/ECommerceApp.AssemblyInfo.cs
+++ test-for-demo/dotnet-ecommerce/src/ECommerceApp/obj/Debug/net8.0/ECommerceApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+854d1505e6f6ad01bfd70ce4d57ff91d321d858f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b294930e6244fe80386c31a0664dc97ef4e16a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
